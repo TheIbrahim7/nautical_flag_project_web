19bf2fd19284f68fa1924d2b3c4ac4f708badcff
Ibrahim Oduola <ibrahim.oduola007@gmail.com>
2023-09-05 21:34:39 +0100

    schema added and needs to be validated

---
+++ app/components/screen/notice.jsx
@@ -0,0 +1,25 @@
+export default function InstallationNotice(){
+    
+    
+    return <section>
+
+        <div>
+
+            <div>
+                <h2>  </h2>
+            </div>
+
+            <p></p>
+
+            <ul>
+                <li>  </li>
+            </ul>
+
+            <p>So what do you say?</p>
+
+            <button>Install</button>
+
+        </div>
+
+    </section>
+}
--- app/layout.js
+++ app/layout.js
@@ -14,14 +14,14 @@ export const metadata = {
     canonical: "https://maritime-flag.vercel.app"
   },
   openGraph:{
-    title: "Learn to Identify the nautical flags and Meanings",
-    description: "App for learning, quick-referencing and Quiz: Learn under 25 mins",
+    title: "Learn to Identify the nautical flags and meanings",
+    description: "App for learning, quick-referencing and Quiz: Learn under 25 mins", //Nautical flags -> know them, quickly check a flag and sometimes quiz yourself on this web app
     url:"https://maritime-flag.vercel.app",
     "type": "website",    
   },
   twitter:{
     card:"summary_large_image",
-    title: "Learn to Identify the nautical flags and Meanings",
+    title: "Learn to Identify the nautical flags and meanings",
     description: "App for learning, quick-referencing and Quiz: Learn under 25 mins",
     creator: "@ioweb_pro",
     site:"@ioweb_pro",
@@ -35,8 +35,50 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    "@type": "SoftwareApplication",
+    "name": "nautical signal flags app",
+    "alternateName": "Maritime Signal FLags",
+    description: "Learn to identify the nautical signal flags, the alphabets and meanings. This web application is built for progressive learning, quick referencing and more.",
+    "url": "https://maritime-flag.vercel.app/",
+    "author": {
+      "@type": "Person",
+      "name": "Ibrahim Oduola",
+      "image": "https://maritime-flag.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fme.c3618e5c.png&w=3840&q=75",
+    },
+    logo:"https://maritime-flag.vercel.app/icon-256x256.png",
+    "offers": {
+      "@type": "Offer",
+      "price": "0"
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "ioweb",
+      "url": "https://www.ioweb.pro",
+    },
+    "applicationCategory": ["EducationalApplication", "WebApplication"],
+    // "downloadUrl": "https://maritime-flag.vercel.app/",
+    "operatingSystem": ["Android", "iOS", "Windows", "MacOS"],
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "4.7",
+      "bestRating": "5",
+      "worstRating": "1",
+      "ratingCount": "6"
+    }
+  }
+
+
   return (
     <html lang="en">
+      
+      {/* Add JSON-LD to your page */}
+      <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
 
       <body className={qs.className}>
 
